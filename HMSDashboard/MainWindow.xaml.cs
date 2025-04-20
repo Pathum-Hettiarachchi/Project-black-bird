130c5d534e228d26a4b89b706b81cde4d7e8e1cd
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-04-21 04:44:06 +1300

    Initial Commit of WPF project

---
+++ HMSDashboard/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace HMSDashboard
+{
+    public partial class MainWindow : Window
+    {
+        private bool isDragging = false;
+        private Point clickPosition;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PatientListPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            clickPosition = e.GetPosition(MainCanvas);
+            PatientListPanel.CaptureMouse();
+        }
+
+        private void PatientListPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                var currentPosition = e.GetPosition(MainCanvas);
+                double deltaX = currentPosition.X - clickPosition.X;
+                double deltaY = currentPosition.Y - clickPosition.Y;
+                double newLeft = Canvas.GetLeft(PatientListPanel) + deltaX;
+                double newTop = Canvas.GetTop(PatientListPanel) + deltaY;
+                Canvas.SetLeft(PatientListPanel, newLeft);
+                Canvas.SetTop(PatientListPanel, newTop);
+                clickPosition = currentPosition;
+            }
+        }
+
+        private void PatientListPanel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            PatientListPanel.ReleaseMouseCapture();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Patient_add patientAddWindow = new Patient_add();
+            patientAddWindow.Show();
+            this.Close();
+
+        }
+    }
+}
---
+++ HMSDashboard/Patient add.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HMSDashboard
+{
+    /// <summary>
+    /// Interaction logic for Patient_add.xaml
+    /// </summary>
+    public partial class Patient_add : Window
+    {
+        public Patient_add()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+
+        }
+    }
+}
